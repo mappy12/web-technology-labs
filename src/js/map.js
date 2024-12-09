fb77c0268322b62f07b976306c5f07442a91df95
mappywin <mappydesuu@gmail.com>
2024-12-10 00:03:50 +0300

    backup 1.4

--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,9 @@
-const map = L.map('map').setView([55.751244, 37.618423], 16);
+const map = L.map('map').setView([55.723121, 37.584297], 16);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-L.marker([55.751244, 37.618423]).addTo(map)
+L.marker([55.723121, 37.584297]).addTo(map)
     .bindPopup('DENEX<br>Our shop is here!')
     .openPopup();
